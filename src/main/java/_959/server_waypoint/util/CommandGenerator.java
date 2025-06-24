1e8b437e360f7f6f120d15e3221f9fc4969dd5bd
2676959 <p2676959@gmail.com>
2025-06-24 16:02:32 -0400

    new generator: addCmd

--- src/main/java/_959/server_waypoint/util/CommandGenerator.java
+++ src/main/java/_959/server_waypoint/util/CommandGenerator.java
@@ -2,28 +2,49 @@ package _959.server_waypoint.util;
 
 import _959.server_waypoint.server.waypoint.SimpleWaypoint;
 import net.minecraft.registry.RegistryKey;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import static _959.server_waypoint.util.TextHelper.colorIndexToName;
+
 public class CommandGenerator {
+    public static final String WAYPOINT_COMMAND = "/wp";
+    public static final String ADD_ARGUMENT = "add";
+    public static final String EDIT_ARGUMENT = "edit";
     public static String tpCmd(RegistryKey<World> dimKey, BlockPos pos, int yaw) {
         return "/execute in %s run tp @s %d %d %d %d 0".formatted(dimKey.getValue().toString(), pos.getX(), pos.getY(), pos.getZ(), yaw);
     }
 
-    public static String editCmd(RegistryKey<World> dimKey, String listName, SimpleWaypoint waypoint) {
-        return "/wp edit %s %s %s %s %d %d %d %s %d %b"
-                .formatted(dimKey.getValue().toString(),
-                        listName,
-                        waypoint.name(),
-                        waypoint.initials(),
+    public static String addCmd(RegistryKey<World> dimKey, String listName, SimpleWaypoint waypoint) {
+        return WAYPOINT_COMMAND + ADD_ARGUMENT + "%s %d %d %d %s %s %s %s %d %b"
+                .formatted(
+                        dimKey.getValue().toString(),
                         waypoint.pos().getX(),
                         waypoint.pos().getY(),
                         waypoint.pos().getZ(),
-                        Formatting.byColorIndex(waypoint.colorIdx()).asString(),
+                        waypoint.name(),
+                        waypoint.initials(),
+                        listName,
+                        colorIndexToName(waypoint.colorIdx()),
                         waypoint.yaw(),
                         waypoint.global()
                 );
+    }
 
+    public static String editCmd(RegistryKey<World> dimKey, String listName, SimpleWaypoint waypoint) {
+        return WAYPOINT_COMMAND + EDIT_ARGUMENT +
+                "%s %s %s %s %d %d %d %s %d %b"
+                        .formatted(
+                                dimKey.getValue().toString(),
+                                listName,
+                                waypoint.name(),
+                                waypoint.initials(),
+                                waypoint.pos().getX(),
+                                waypoint.pos().getY(),
+                                waypoint.pos().getZ(),
+                                colorIndexToName(waypoint.colorIdx()),
+                                waypoint.yaw(),
+                                waypoint.global()
+                        );
     }
 }
