14ff36ae70ac740bfeff9e755a33151ac4b46fbc
2676959 <p2676959@gmail.com>
2025-07-03 02:13:35 -0400

    Xaero's minimap is not a required dependency when this mod is installed on serverside.

---
+++ src/main/java/_959/server_waypoint/util/DimensionFileHelper.java
@@ -0,0 +1,40 @@
+package _959.server_waypoint.util;
+
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+
+public class DimensionFileHelper {
+    public static String getFileName(RegistryKey<World> dimKey) {
+        if (dimKey == World.OVERWORLD) {
+            return "dim%0";
+        } else if (dimKey == World.NETHER) {
+            return "dim%-1";
+        } else if (dimKey == World.END) {
+            return "dim%1";
+        } else {
+            Identifier identifier = dimKey.getValue();
+            return "dim%" + identifier.getNamespace() + "$" + identifier.getPath().replace('/', '%');
+        }
+    }
+
+    public static RegistryKey<World> getDimensionKey(String folderName) {
+        String dimId = folderName.substring(4);
+        switch (dimId) {
+            case "0" -> {
+                return World.OVERWORLD;
+            }
+            case "1" -> {
+                return World.END;
+            }
+            case "-1" -> {
+                return World.NETHER;
+            }
+            default -> {
+                String[] idParts = dimId.split("\\$");
+                return idParts.length < 2 ? null : RegistryKey.of(RegistryKeys.WORLD, Identifier.of(idParts[0], idParts[1].replace('%', '/')));
+            }
+        }
+    }
+}
