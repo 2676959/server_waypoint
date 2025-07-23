9e64606fe1326fbf868d600548e3e8f1cd96f26f
2676959 <p2676959@gmail.com>
2025-07-23 11:29:01 -0400

    split platform sources when building

--- src/main/java/_959/server_waypoint/neoforge/ServerWaypointNeoforge.java
+++ src/main/java/_959/server_waypoint/neoforge/ServerWaypointNeoforge.java
@@ -1,10 +1,12 @@
 //? if neoforge {
 /*package _959.server_waypoint.neoforge;
 
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 
-@Mod("sever_waypoint")
+@Mod(value = ServerWaypointNeoforge.MODID, dist = Dist.DEDICATED_SERVER)
 public class ServerWaypointNeoforge {
+    public static final String MODID = "server_waypoint";
     public ServerWaypointNeoforge() {}
 }
 *///?}
