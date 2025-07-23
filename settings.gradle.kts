a9e60caaba945476272589fd6d59244e4098bdd0
2676959 <p2676959@gmail.com>
2025-07-22 20:21:51 -0400

    add neoforge development support

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+	repositories {
+		mavenCentral()
+		gradlePluginPortal()
+		maven("https://maven.fabricmc.net/")
+		maven("https://maven.architectury.dev")
+		maven("https://maven.neoforged.net/releases/")
+		maven("https://maven.kikugie.dev/snapshots")
+	}
+}
+
+plugins {
+	id("dev.kikugie.stonecutter") version "0.7.1"
+}
+
+stonecutter {
+	kotlinController = true
+	centralScript = "build.gradle.kts"
+
+	// Subproject configuration
+	shared {
+		fun mc(loader: String, vararg versions: String) {
+			for (version in versions) vers("$version-$loader", version)
+		}
+		mc("fabric", "1.21", "1.21.3", "1.21.5")
+		mc("neoforge", "1.21", "1.21.3", "1.21.5")
+	}
+	create(rootProject)
+}
+
+rootProject.name = "server_waypoint"
---
+++ src/main/java/_959/server_waypoint/neoforge/ServerWaypointNeoforge.java
@@ -0,0 +1,10 @@
+//? if neoforge {
+/*package _959.server_waypoint.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod("sever_waypoint")
+public class ServerWaypointNeoforge {
+    public ServerWaypointNeoforge() {}
+}
+*///?}
