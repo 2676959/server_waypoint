f0152425bfa949c972297e92990c8634e6000da1
2676959 <p2676959@gmail.com>
2026-02-02 00:31:46 -0500

    client configuration class

---
+++ src/main/java/_959/server_waypoint/common/client/ClientConfig.java
@@ -0,0 +1,43 @@
+package _959.server_waypoint.common.client;
+
+import _959.server_waypoint.common.client.render.OptimizedWaypointRenderer;
+import com.google.gson.annotations.Expose;
+
+public class ClientConfig {
+    @Expose private int viewDistance = 12;
+    @Expose private boolean enableWaypointRender = true;
+    @Expose private boolean autoSyncToXaerosMinimap = true;
+    private int squaredViewDistanceInBlocks = 12 * 16 * 12 * 16;
+
+    private ClientConfig() {}
+
+    public boolean isEnableWaypointRender() {
+        return enableWaypointRender;
+    }
+
+    public void setEnableWaypointRender(boolean enableWaypointRender) {
+        this.enableWaypointRender = enableWaypointRender;
+        OptimizedWaypointRenderer.enableRendering(enableWaypointRender);
+    }
+
+    public boolean isAutoSyncToXaerosMinimap() {
+        return autoSyncToXaerosMinimap;
+    }
+
+    public void setAutoSyncToXaerosMinimap(boolean autoSyncToXaerosMinimap) {
+        this.autoSyncToXaerosMinimap = autoSyncToXaerosMinimap;
+    }
+
+    public int getViewDistance() {
+        return viewDistance;
+    }
+
+    public void setViewDistance(int viewDistance) {
+        this.viewDistance = viewDistance;
+        this.squaredViewDistanceInBlocks = viewDistance * viewDistance * 16 * 16;
+    }
+
+    public int getSquaredViewDistanceInBlocks() {
+        return this.squaredViewDistanceInBlocks;
+    }
+}
