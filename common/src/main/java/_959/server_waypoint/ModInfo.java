8b41e1a51e3715aa58ea7c8214036ec10418ca11
2676959 <p2676959@gmail.com>
2026-02-05 08:18:00 -0500

    Update version to 3.0.0 and refine mod name

--- common/src/main/java/_959/server_waypoint/ModInfo.java
+++ common/src/main/java/_959/server_waypoint/ModInfo.java
@@ -5,7 +5,7 @@ package _959.server_waypoint;
  * */
 public final class ModInfo {
     public static final String MOD_ID = "server_waypoint";
-    public static final String MOD_NAME = "ServerWaypoint";
-    public static final String MOD_VERSION = "2.8.4";
+    public static final String MOD_NAME = "Server Waypoint";
+    public static final String MOD_VERSION = "3.0.0";
     public static final String DOWNLOAD_URL = "https://modrinth.com/plugin/server_waypoint/versions";
 }
