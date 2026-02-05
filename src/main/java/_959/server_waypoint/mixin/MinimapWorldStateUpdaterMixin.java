a224e05cd07e69292c6dc2abede42e6a77e8e42b
2676959 <p2676959@gmail.com>
2026-02-05 08:15:50 -0500

    Update Xaero's Minimap synchronization logic

--- src/main/java/_959/server_waypoint/mixin/MinimapWorldStateUpdaterMixin.java
+++ src/main/java/_959/server_waypoint/mixin/MinimapWorldStateUpdaterMixin.java
@@ -1,31 +1,23 @@
 package _959.server_waypoint.mixin;
 
-import _959.server_waypoint.common.client.WaypointClientMod;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xaero.hud.minimap.world.state.MinimapWorldStateUpdater;
 
-import static _959.server_waypoint.common.client.WaypointClientMod.LOGGER;
-
-//? if fabric {
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-//?} elif neoforge {
-/*import net.neoforged.neoforge.network.PacketDistributor;
-*///?}
+import static _959.server_waypoint.common.client.WaypointClientMod.*;
+import static _959.server_waypoint.common.client.WaypointClientMod.ClientNetworkState.SYNC_FINISHED;
+import static _959.server_waypoint.common.client.handlers.HandlerForXaerosMinimap.syncFromServerWaypointMod;
 
 @Mixin(MinimapWorldStateUpdater.class)
 public class MinimapWorldStateUpdaterMixin {
 
     @Inject(method = "onServerLevelId", at = @At(value = "TAIL"), remap = false)
     private void injectOnServerLevelId(int id, CallbackInfo ci) {
-
-        //? if fabric {
-        ClientPlayNetworking.send(WaypointClientMod.getInstance().getClientHandShakePayload());
-        //?} elif neoforge {
-        /*PacketDistributor.sendToServer(HandshakePayloadGenerator.generate());
-        *///?}
+        isXaerosMinimapReady = true;
+        if (getClientConfig().isAutoSyncToXaerosMinimap() && getNetworkState().equals(SYNC_FINISHED)) {
+            syncFromServerWaypointMod();
+        }
     }
 }
