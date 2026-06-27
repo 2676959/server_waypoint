259cc4349af26f8cc7283d068343a06287a66c0f
2676959 <p2676959@gmail.com>
2026-06-27 00:36:56 -0400

    add synced.png

--- mods/src/main/java/_959/server_waypoint/common/client/gui/WaypointTextures.java
+++ mods/src/main/java/_959/server_waypoint/common/client/gui/WaypointTextures.java
@@ -10,39 +10,51 @@ public final class WaypointTextures {
     //$ resource_location_type_swap
     Identifier
     SHOW_ICON = modId("textures/gui/show.png");
+
     public static final
     //$ resource_location_type_swap
     Identifier
     HIDE_ICON = modId("textures/gui/hide.png");
+
     public static final
     //$ resource_location_type_swap
     Identifier
     ADD_ICON = modId("textures/gui/add.png");
+
     public static final
     //$ resource_location_type_swap
     Identifier
     EDIT_ICON = modId("textures/gui/edit.png");
+
     public static final
     //$ resource_location_type_swap
     Identifier
     REMOVE_ICON = modId("textures/gui/delete.png");
+
     public static final
     //$ resource_location_type_swap
     Identifier
     CONFIRM_REMOVE_ICON = modId("textures/gui/confirm_delete.png");
+
     public static final
     //$ resource_location_type_swap
     Identifier
     LIST_EMPTY = modId("textures/gui/list_empty.png");
+
     public static final
     //$ resource_location_type_swap
     Identifier
     LIST_EXPAND_ICON = modId("textures/gui/list_expand.png");
+
     public static final
     //$ resource_location_type_swap
     Identifier
     LIST_COLLAPSE_ICON = modId("textures/gui/list_collapse.png");
 
+    public static final
+    //$ resource_location_type_swap
+    Identifier
+    SYNCED_ICON = modId("textures/gui/synced.png");
     private WaypointTextures() {
     }
 }
